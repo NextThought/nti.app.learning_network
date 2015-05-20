f48b242774c6cfbe67d90b55598974b1e4720bc5
Josh Zuech <josh.zuech@nextthought.com>
2015-05-20 15:13:27 +0000

    Add admin views for getting learning stats by user/course
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.learning_network/trunk@65645 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/learning_network/admin_views.py
+++ src/nti/app/learning_network/admin_views.py
@@ -20,11 +20,12 @@ from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.common.maps import CaseInsensitiveDict
 
-#from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseEnrollments
 
 from nti.dataserver import authorization as nauth
-
 from nti.dataserver.interfaces import IUser
+from nti.dataserver.users.users import User
 
 from nti.externalization.interfaces import LocatedExternalDict
 
@@ -44,16 +45,69 @@ def _get_stat_source( iface, user, course, timestamp ):
 		stats_source = iface( user )
 	return stats_source
 
+def _add_stats_to_user_dict( user_dict, user, course, timestamp ):
+	access_source = _get_stat_source( IAccessStatsSource, user, course, timestamp )
+	prod_source = _get_stat_source( IProductionStatsSource, user, course, timestamp )
+	user_dict['Access'] = access_source
+	user_dict['Production'] = prod_source
+
+@view_config(	route_name='objects.generic.traversal',
+				renderer='rest',
+				request_method='GET',
+				context=ICourseInstance,
+				permission=nauth.ACT_NTI_ADMIN,
+				name=STATS_VIEW_NAME )
+class LearningNetworkCourseStats( AbstractAuthenticatedView ):
+	"""
+	For the given course (and possibly user or timestamp), return
+	the learning network stats for each user enrolled in the course.
+	"""
+
+	def __call__(self):
+		# For beer-200, 3k students, 650s (5 students/s) with 55k loads.
+		result = LocatedExternalDict()
+		course = self.context
+		params = CaseInsensitiveDict( self.request.params )
+		username = params.get( 'User' )
+		timestamp = params.get( 'Timestamp' )
+		timestamp = datetime.utcfromtimestamp( timestamp ) if timestamp else None
+
+		user = None
+		usernames = ()
+
+		if username:
+			user = User.get_user( username )
+			if user is None:
+				return hexc.HTTPNotFound( "No user found %s" % username )
+
+			usernames = (username,)
+		else:
+			usernames = tuple( ICourseEnrollments( course ).iter_principals() )
+
+		for username in usernames:
+			result[username] = user_dict = {}
+			user = User.get_user( username )
+			if user is not None:
+				_add_stats_to_user_dict( user_dict, user, course, timestamp )
+			else:
+				logger.info( 'User (%s) in course not found.', username )
+
+		result['ItemCount'] = len( usernames )
+		return result
+
 @view_config(	route_name='objects.generic.traversal',
 				renderer='rest',
 				request_method='GET',
 				context=IUser,
 				permission=nauth.ACT_NTI_ADMIN,
 				name=STATS_VIEW_NAME )
-class LearningNetworkStats( AbstractAuthenticatedView ):
+class LearningNetworkUserStats( AbstractAuthenticatedView ):
+	"""
+	For the given user (and possibly course or timestamp), return
+	the learning network stats.
+	"""
 
 	def __call__(self):
-		# TODO Filter by user, course, timestamp, stat type
 		user = self.context
 		params = CaseInsensitiveDict( self.request.params )
 		course_ntiid = params.get( 'Course' )
@@ -63,13 +117,10 @@ class LearningNetworkStats( AbstractAuthenticatedView ):
 		course = None
 		if course_ntiid:
 			course = find_object_with_ntiid( course_ntiid )
+			course = ICourseInstance( course, None )
 			if course is None:
 				return hexc.HTTPNotFound( "No course found for %s" % course_ntiid )
 
-		access_source = _get_stat_source( IAccessStatsSource, user, course, timestamp )
-		prod_source = _get_stat_source( IProductionStatsSource, user, course, timestamp )
-
 		result = LocatedExternalDict()
-		result['Access'] = access_source
-		result['Production'] = prod_source
+		_add_stats_to_user_dict( result, user, course, timestamp )
 		return result
