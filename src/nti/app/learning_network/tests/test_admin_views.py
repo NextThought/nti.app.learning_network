7c23d3bb7f6817c02dd19563a3778ccb63b2946b
Josh Zuech <josh.zuech@nextthought.com>
2015-05-22 03:41:27 +0000

    Admin view tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.learning_network/trunk@65869 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/learning_network/tests/test_admin_views.py
+++ src/nti/app/learning_network/tests/test_admin_views.py
@@ -7,24 +7,16 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-from hamcrest import is_
 from hamcrest import not_none
+from hamcrest import has_entry
 from hamcrest import assert_that
-from hamcrest import less_than_or_equal_to
-
-from nti.analytics.interfaces import IUserResearchStatus
-
-from nti.app.analytics.views import SET_RESEARCH_VIEW
 
 from nti.dataserver.users import User
 
 import nti.dataserver.tests.mock_dataserver as mock_dataserver
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
-from nti.app.testing.decorators import WithSharedApplicationMockDSHandleChanges
 from nti.app.testing.application_webtest import ApplicationLayerTest
-
-from . import LearningNetworkApplicationTestLayer
 
 from ..admin_views import STATS_VIEW_NAME
 
@@ -32,9 +24,17 @@ class TestAppLearningNetwork( ApplicationLayerTest ):
 
 	@WithSharedApplicationMockDS(testapp=True, users=True)
 	def test_stats_view(self):
+		"""
+		Get learning stats for a user.
+		"""
 		with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
 			user = User.create_user( 	username='new_user1', dataserver=self.ds,
 										external_value={'realname':'Jim Bob', 'email': 'foo@bar.com'} )
 
-		url = '/dataserver2/users/new_user1/' + STATS_VIEW_NAME
-		results = self.testapp.get( url )
+			url = '/dataserver2/users/%s/%s' % (user.username, STATS_VIEW_NAME)
+		result = self.testapp.get( url )
+		body = result.json_body
+		assert_that( body, has_entry( 'Access', not_none() ))
+		assert_that( body, has_entry( 'Production', not_none() ))
+		assert_that( body, has_entry( 'Interaction', not_none() ))
+
