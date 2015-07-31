90fff73c120c7da60af1e090e2b296832211a71a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-31 20:11:49 +0000

    adjust filter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.learning_network/trunk@69812 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/learning_network/filters.py
+++ src/nti/app/learning_network/filters.py
@@ -16,13 +16,14 @@ from nti.appserver.interfaces import ICreatableObjectFilter
 @interface.implementer(ICreatableObjectFilter)
 class _LearningNetworkContentObjectFilter(object):
 
-	PREFIX = 'application/vnd.nextthought.learningnetwork'
+	PREFIX_1 = u'application/vnd.nextthought.learningnetwork'
+	PREFIX_2 = u'application/vnd.nextthought.learning_network'
 
 	def __init__(self, context=None):
 		pass
 
 	def filter_creatable_objects(self, terms):
 		for name in list(terms):  # mutating
-			if name.startswith(self.PREFIX):
+			if name.startswith(self.PREFIX) or name.startswith(self.PREFIX_2):
 				terms.pop(name, None)
 		return terms
