6fce9432c5e57a64c1fbae4c7f7b4de8248a9173
Josh Zuech <josh.zuech@nextthought.com>
2015-08-04 19:54:46 +0000

    Add admin view to get stats in csv
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.learning_network/trunk@69945 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/learning_network/admin_views.py
+++ src/nti/app/learning_network/admin_views.py
@@ -9,6 +9,10 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import csv
+
+from io import BytesIO
+
 from datetime import datetime
 
 from zope import component
@@ -20,15 +24,17 @@ from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.common.maps import CaseInsensitiveDict
 
+from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseEnrollments
 
-from nti.dataserver.interfaces import IUser
+from nti.dataserver.interfaces import IUser, IDataserverFolder
 from nti.dataserver.users.users import User
 from nti.dataserver import authorization as nauth
 
 from nti.externalization.interfaces import LocatedExternalDict
 
+from nti.learning_network.interfaces import IStats
 from nti.learning_network.interfaces import IAccessStatsSource
 from nti.learning_network.interfaces import IProductionStatsSource
 from nti.learning_network.interfaces import IInteractionStatsSource
@@ -49,13 +55,117 @@ def _get_stat_source(iface, user, course, timestamp):
 		stats_source = iface(user, None)
 	return stats_source
 
-def _add_stats_to_user_dict(user_dict, user, course, timestamp):
+def _get_stats_for_user( user, course, timestamp=None ):
 	access_source = _get_stat_source(IAccessStatsSource, user, course, timestamp)
 	prod_source = _get_stat_source(IProductionStatsSource, user, course, timestamp)
 	social_source = _get_stat_source(IInteractionStatsSource, user, course, timestamp)
+	return access_source, prod_source, social_source
+
+def _add_stats_to_user_dict(user_dict, user, course, timestamp):
+	access_source, prod_source, social_source = _get_stats_for_user( user, course, timestamp )
 	user_dict['Access'] = access_source
 	user_dict['Production'] = prod_source
 	user_dict['Interaction'] = social_source
+
+@view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 request_method='GET',
+			 context=IDataserverFolder,
+			 permission=nauth.ACT_NTI_ADMIN,
+			 name=STATS_VIEW_NAME)
+class LearningNetworkCSVStats(AbstractAuthenticatedView):
+
+	type_stat_statvar_map = None
+
+	def _get_source_str(self, source):
+		if IAccessStatsSource.providedBy( source ):
+			return 'Access'
+		if IProductionStatsSource.providedBy( source ):
+			return 'Production'
+		if IInteractionStatsSource.providedBy( source ):
+			return 'Interaction'
+
+	def _get_headers( self, writer, *sources ):
+		"""
+		Build up a consistent map of type->stat->stat_field so
+		our data points match up consistently for all users.
+		"""
+		if not self.type_stat_statvar_map:
+			type_stat_statvar_map = {}
+			for source in sources:
+				source_type = self._get_source_str( source )
+				type_stat_statvar_map[ source_type ] = stat_map = {}
+				for source_var in dir( source ):
+					if source_var.startswith( '_' ):
+						continue
+					stat = getattr( source, source_var )
+					if IStats.providedBy( stat ):
+						stat_map[ source_var ] = source_stats = []
+
+						for stat_var in vars( stat ):
+							# How do we get 'parameters'?
+							if 		not stat_var.startswith( '_' ) \
+								and stat_var != 'parameters':
+								source_stats.append( stat_var )
+
+			self.type_stat_statvar_map = type_stat_statvar_map
+
+			# Now build our headers (match iteration with stat iteration)
+			header_labels = []
+			for source in sources:
+				source_type = self._get_source_str( source )
+				stat_map = type_stat_statvar_map.get( source_type  )
+				for stat_name, stat_vars in stat_map.items():
+					for stat_var in stat_vars:
+						header_labels.append( '%s_%s_%s' % ( source_type, stat_name, stat_var ))
+			writer.writerow( header_labels )
+		return self.type_stat_statvar_map
+
+	def _write_stats_for_user( self, writer, user, course ):
+		sources = _get_stats_for_user( user, course )
+		type_stat_statvar_map = self._get_headers( writer, *sources )
+		user_results = []
+		for source in sources:
+			stat_map = type_stat_statvar_map.get( self._get_source_str( source )  )
+			for stat_name, stat_vars in stat_map.items():
+				stat = getattr( source, stat_name )
+				for stat_var in stat_vars:
+					stat_value = getattr( stat, stat_var )
+					user_results.append( stat_value )
+
+		writer.writerow( user_results )
+
+	def __call__(self):
+		course = self.context
+		params = CaseInsensitiveDict(self.request.params)
+		course_filter = params.get( 'filter', '' )
+
+		response = self.request.response
+		response.content_encoding = str( 'identity' )
+		response.content_type = str('text/csv; charset=UTF-8')
+		filename = 'lstd_learning_network_stats.csv'
+		response.content_disposition = str( 'attachment; filename="%s"' % filename )
+		stream = BytesIO()
+		writer = csv.writer(stream)
+
+		catalog = component.getUtility( ICourseCatalog )
+		for entry in catalog.iterCatalogEntries():
+			course = ICourseInstance( entry, None )
+			if 		course is None \
+				or 	course_filter not in entry.ProviderUniqueID:
+				continue
+			usernames = tuple(ICourseEnrollments(course).iter_principals())
+
+			for username in usernames:
+				user = User.get_user(username)
+				if 		user is not None \
+					and not username.endswith( '@nextthought.com' ):
+					self._write_stats_for_user( writer, user, course )
+
+		stream.flush()
+		stream.seek(0)
+		response.body_file = stream
+		return response
 
 @view_config(route_name='objects.generic.traversal',
 			 renderer='rest',
@@ -76,6 +186,7 @@ class LearningNetworkCourseStats(AbstractAuthenticatedView):
 		params = CaseInsensitiveDict(self.request.params)
 		username = params.get('Username')
 		timestamp = params.get('Timestamp')
+
 		timestamp = datetime.utcfromtimestamp(timestamp) if timestamp else None
 
 		user = None
