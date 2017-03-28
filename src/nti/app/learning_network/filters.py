1763a96775358fd6e04bd5999e4623d76af9add4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-28 18:29:09 +0000

    remove deprecation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.learning_network/trunk@109790 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/learning_network/filters.py
+++ src/nti/app/learning_network/filters.py
@@ -11,19 +11,20 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
-from nti.appserver.interfaces import ICreatableObjectFilter
+from nti.dataserver.interfaces import ICreatableObjectFilter
+
 
 @interface.implementer(ICreatableObjectFilter)
 class _LearningNetworkContentObjectFilter(object):
 
-	PREFIX_1 = u'application/vnd.nextthought.learningnetwork'
-	PREFIX_2 = u'application/vnd.nextthought.learning_network'
+    PREFIX_1 = u'application/vnd.nextthought.learningnetwork'
+    PREFIX_2 = u'application/vnd.nextthought.learning_network'
 
-	def __init__(self, context=None):
-		pass
+    def __init__(self, context=None):
+        pass
 
-	def filter_creatable_objects(self, terms):
-		for name in tuple(terms):  # mutating
-			if name.startswith(self.PREFIX_1) or name.startswith(self.PREFIX_2):
-				terms.pop(name, None)
-		return terms
+    def filter_creatable_objects(self, terms):
+        for name in tuple(terms):  # mutating
+            if name.startswith(self.PREFIX_1) or name.startswith(self.PREFIX_2):
+                terms.pop(name, None)
+        return terms
