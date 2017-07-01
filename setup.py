5bf72f479065d216a9c0897b316efc686a92b4f4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-01 15:31:37 +0000

    bump graphviz
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.learning_network/trunk@116402 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,40 +4,38 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.app',
-	],
-	'console_scripts': [
-	],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+    'console_scripts': [
+    ],
 }
 
-import platform
-py_impl = getattr(platform, 'python_implementation', lambda: None)
-IS_PYPY = py_impl() == 'PyPy'
 
 setup(
-	name='nti.app.learning_network',
-	version=VERSION,
-	author='Josh Zuech',
-	author_email='josh.zuech@nextthought.com',
-	description="NTI app learning_network",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid preference',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app'],
-	install_requires=[
-		'setuptools',
-		'collective.monkeypatcher',
-		'nti.learning_network'
-	],
-	entry_points=entry_points
+    name='nti.app.learning_network',
+    version=VERSION,
+    author='Josh Zuech',
+    author_email='josh.zuech@nextthought.com',
+    description="NTI app learning_network",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='pyramid preference',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti', 'nti.app'],
+    install_requires=[
+        'setuptools',
+        'collective.monkeypatcher',
+        'nti.learning_network',
+    ],
+    entry_points=entry_points
 )
